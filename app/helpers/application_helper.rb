516121031c7c6d8777bae2f015406ed46618efa0
PHAN QUANG LAM <lam.phan.qg@gmail.com>
2021-11-09 21:20:16 +0900

    Modify alert display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def flash_alert
+    capture do
+      if notice.present?
+        concat(content_tag(:div, class: "alert alert-primary alert-dismissible fade show", role: "alert") do
+          concat notice.to_s
+          concat button_tag("", class: "btn-close", "data-bs-dismiss": "alert", "aria-label": "Close")
+        end)
+      end
+
+      if alert.present?
+        concat(content_tag(:div, class: "alert alert-danger alert-dismissible fade show", role: "alert") do
+          concat alert.to_s
+          concat button_tag("", class: "btn-close", "data-bs-dismiss": "alert", "aria-label": "Close")
+        end)
+      end
+    end
+  end
 end
